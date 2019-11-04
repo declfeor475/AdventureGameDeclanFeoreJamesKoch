335daf6d6a2dcc9cbd2995f74ef73cb4c43b0f71
declfeor475 <declfeor475@ed.amdsb.ca>
2019-11-04 12:18:44 -0500

    Add project files.

---
+++ AdventureGameDeclanFeoreJamesKoch/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AdventureGameDeclanFeoreJamesKoch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.sceneImage = new System.Windows.Forms.PictureBox();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.nLabel = new System.Windows.Forms.Label();
+            this.mLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(13, 13);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(872, 101);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.Location = new System.Drawing.Point(139, 537);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(587, 46);
+            this.redLabel.TabIndex = 1;
+            this.redLabel.Text = "label1";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Location = new System.Drawing.Point(139, 611);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(587, 46);
+            this.blueLabel.TabIndex = 3;
+            this.blueLabel.Text = "label2";
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.Location = new System.Drawing.Point(139, 680);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(587, 46);
+            this.yellowLabel.TabIndex = 5;
+            this.yellowLabel.Text = "label3";
+            // 
+            // sceneImage
+            // 
+            this.sceneImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.sceneImage.Location = new System.Drawing.Point(7, 137);
+            this.sceneImage.Name = "sceneImage";
+            this.sceneImage.Size = new System.Drawing.Size(878, 358);
+            this.sceneImage.TabIndex = 7;
+            this.sceneImage.TabStop = false;
+            // 
+            // bLabel
+            // 
+            this.bLabel.BackColor = System.Drawing.Color.Red;
+            this.bLabel.Location = new System.Drawing.Point(83, 537);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(50, 46);
+            this.bLabel.TabIndex = 8;
+            this.bLabel.Text = "B";
+            this.bLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nLabel
+            // 
+            this.nLabel.BackColor = System.Drawing.Color.RoyalBlue;
+            this.nLabel.Location = new System.Drawing.Point(83, 611);
+            this.nLabel.Name = "nLabel";
+            this.nLabel.Size = new System.Drawing.Size(50, 46);
+            this.nLabel.TabIndex = 9;
+            this.nLabel.Text = "N";
+            this.nLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mLabel
+            // 
+            this.mLabel.BackColor = System.Drawing.Color.Yellow;
+            this.mLabel.Location = new System.Drawing.Point(83, 680);
+            this.mLabel.Name = "mLabel";
+            this.mLabel.Size = new System.Drawing.Size(50, 46);
+            this.mLabel.TabIndex = 10;
+            this.mLabel.Text = "M";
+            this.mLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 752);
+            this.Controls.Add(this.mLabel);
+            this.Controls.Add(this.nLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.sceneImage);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Going to a Friends";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.PictureBox sceneImage;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label nLabel;
+        private System.Windows.Forms.Label mLabel;
+    }
+}
+
---
+++ AdventureGameDeclanFeoreJamesKoch/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+
+namespace AdventureGameDeclanFeoreJamesKoch
+{
+    public partial class Form1 : Form
+    {
+        // tracks what part of the game the user is at
+        int scene = 0;
+
+        // random number generator
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //button 
+            
+
+            //display initial message and options
+            outputLabel.Text = "You want to go to your friend's house. What mode of transportation will you use.";
+            redLabel.Text = "Walk";
+            blueLabel.Text = "Drive";
+            yellowLabel.Text = "Skip driving, stay inside, and don't hangout with anyone";
+            sceneImage.BackgroundImage = Properties.Resources.case_0;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance to the next appropriate scene.
+            if (e.KeyCode == Keys.B) //red button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 2) { scene = 5; }
+                else if (scene == 3) { scene = 7; }
+                else if (scene == 4) { scene = 8; }
+                else if (scene == 5) { scene = 10; }
+                else if (scene == 6) { scene = 5; }
+                else if (scene == 7) { scene = 12; }
+                else if (scene == 8) { scene = 0; }
+                else if (scene == 9) { scene = 14; }
+                else if (scene == 10) { scene = 16; }
+                else if (scene == 11) { scene = 0; }
+                else if (scene == 12) { scene = 99; }
+                else if (scene == 13) { scene = 0; }
+                else if (scene == 14) { scene = 0; }
+                else if (scene == 15) { scene = 99; }
+                else if (scene == 16) { scene = 18; }
+                else if (scene == 17) { scene = 20; }
+                else if (scene == 18) { scene = 0; }
+                else if (scene == 19) { scene = 99; }
+                else if (scene == 20) { scene = 99; }
+                else if (scene == 21) { scene = 0; }
+                else if (scene == 99) { scene = 100; }
+                else if (scene == 100) { scene = 0; }
+                else if (scene == 101) { scene = 0; }
+            }
+            else if (e.KeyCode == Keys.N) //blue button press
+            {
+                if (scene == 0) { scene = 2; }
+                else if (scene == 1) { scene = 4; }
+                else if (scene == 2) { scene = 6; }
+                else if (scene == 3) { scene = 8; }
+                else if (scene == 4) { scene = 9; }
+                else if (scene == 5) { scene = 11; }
+                else if (scene == 6) { this.Close(); }
+                else if (scene == 7) { scene = 13; }
+                else if (scene == 8) { this.Close(); }
+                else if (scene == 9) { scene = 15; }
+                else if (scene == 10) { scene = 17; }
+                else if (scene == 11) { this.Close(); }
+                else if (scene == 13) { this.Close(); }
+                else if (scene == 14) { this.Close(); }
+                else if (scene == 16) { scene = 19; }
+                else if (scene == 17) { scene = 21; }
+                else if (scene == 18) { this.Close(); }
+                else if (scene == 21) { this.Close(); }
+                else if (scene == 99) { scene = 101; }
+                else if (scene == 100) { this.Close(); }
+                else if (scene == 101) { this.Close(); }
+            }
+            else if (e.KeyCode == Keys.M) //yellow button press
+            {
+                scene = 0;
+            }
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "You want to go to a friends house. What mode of transportatation do you use?";
+                    redLabel.Text = "Walk";
+                    blueLabel.Text = "Drive";
+                    yellowLabel.Text = "Skip driving, stay inside, and don't hangout with anyone";
+                    sceneImage.BackgroundImage = Properties.Resources.case_0;
+
+                    break;
+                case 1:
+                    outputLabel.Text = "Do you walk the long safe way or the short scary way?";
+                    redLabel.Text = "Long Safe Way";
+                    blueLabel.Text = "Short Scary Way";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_1_real;
+
+
+                    break;
+                case 2:
+                    outputLabel.Text = "What car do you drive? 2002 Honda Civic or 2019 Ferrari";
+                    redLabel.Text = "2002 Honda Civic";
+                    blueLabel.Text = "2019 Ferraria";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_2;
+                    break;
+                case 3:
+                    outputLabel.Text = "It is taking too long just walking and you become impatient. Do you begin to run or continue walking?";
+                    redLabel.Text = "Walk";
+                    blueLabel.Text = "Run";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_3_realerson;
+                    break;
+                case 4:
+                    outputLabel.Text = "You are walking down by the river and hear 'help!'. Do you check it out or run away?";
+                    redLabel.Text = "Run away";
+                    blueLabel.Text = "Check it out";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_4;
+
+
+                    break;
+                case 5:
+                    outputLabel.Text = "Do you go get food on your way to your friends, or skip food and go right to your friends?";
+                    redLabel.Text = "Get food";
+                    blueLabel.Text = "Skip food";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_5;
+                    break;
+                case 6:
+                    int chance = randGen.Next(1, 11);
+                    if (chance < 8)
+                    { outputLabel.Text = "You chose the Ferrari!"; redLabel.Text = "Continue"; blueLabel.Text = ""; yellowLabel.Text = ""; }
+                    else
+                    { outputLabel.Text = "You crashed the car and died."; redLabel.Text = "Play again"; blueLabel.Text = "Exit"; yellowLabel.Text = ""; }
+                    sceneImage.BackgroundImage = Properties.Resources.case_6;
+                    break;
+                case 7:
+                    outputLabel.Text = "You get thirsty and see a puddle on the road. Do you take a drink from it?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_7;
+                    break;
+                case 8:
+                    outputLabel.Text = "You trip over a stick, hit your head, and internally bleed to death! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_8;
+                    break;
+                case 9:
+                    outputLabel.Text = "Oh my goodness! It is a screaming goat! Do you pet it?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_9;
+                    break;
+                case 10:
+                    outputLabel.Text = "What food store do you go to? Subway or Louisiana Chicken?";
+                    redLabel.Text = "Subway";
+                    blueLabel.Text = "Louisiana Chicken";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_10;
+                    break;
+                case 11:
+                    outputLabel.Text = "You die of hunger! Play again?";
+                    redLabel.Text = "Play Again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_11;
+                    break;
+                case 12:
+                    outputLabel.Text = "You make it too your friends house nice and hydrated.";
+                    redLabel.Text = "Continue";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_12;
+                    break;
+                case 13:
+                    outputLabel.Text = "You die of thirst! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_13;
+                    break;
+                case 14:
+                    outputLabel.Text = "The goat gets mad and eats you painfully! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_14;
+                    break;
+                case 15:
+                    outputLabel.Text = "You continue walking to your friends house, but the goat stares at you menacingly.";
+                    redLabel.Text = "Continue";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_15_real;
+                    break;
+                case 16:
+                    outputLabel.Text = "Do you get a tuna sub or BLT?";
+                    redLabel.Text = "Tuna sub";
+                    blueLabel.Text = "BLT";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_16_realerson;
+
+                    break;
+                case 17:
+                    outputLabel.Text = "Do you get Popcorn chicken or Chicken tenders? ";
+                    redLabel.Text = "Popcorn chicken";
+                    blueLabel.Text = "Chicken tenders";
+                    yellowLabel.Text = "";
+                        sceneImage.BackgroundImage = Properties.Resources.case_17;
+                    break;
+                case 18:
+                    outputLabel.Text = "You die of mercury poisoning! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case18;
+                    break;
+                case 19:
+                    outputLabel.Text = "After eating you drive to your friends.";
+                    redLabel.Text = "Continue";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_18;
+                    break;
+                case 20:
+                    outputLabel.Text = "The popcorn chicken is so good and filling!";
+                    redLabel.Text = "Continue";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.Shridar_Patel;
+                    break;
+                case 21:
+                    outputLabel.Text = "The chicken tenders are under cooked, and you get salmonella poisoning and die! Play again";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_21;
+                    break;
+                case 99:
+                    outputLabel.Text = "When you get there, do you walk right in or knock on the door first?";
+                    redLabel.Text = "Walk right in";
+                    blueLabel.Text = "Knock first";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_99;
+                    break;
+                case 100:
+                    outputLabel.Text = "You get shot by your friend because he confused you for an intruder! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_100;
+                    break;
+                case 101:
+                    outputLabel.Text = "You have fun with your friend! Play again?";
+                    redLabel.Text = "Play again";
+                    blueLabel.Text = "Exit";
+                    yellowLabel.Text = "";
+                    sceneImage.BackgroundImage = Properties.Resources.case_101;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

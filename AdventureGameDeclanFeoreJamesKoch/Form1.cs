ccf76d2bbbe0e0e6e217a574a4ef7e5b1c1bf992
declfeor475 <declfeor475@ed.amdsb.ca>
2019-11-06 13:10:29 -0500

    Final Edits

--- AdventureGameDeclanFeoreJamesKoch/Form1.cs
+++ AdventureGameDeclanFeoreJamesKoch/Form1.cs
@@ -15,13 +15,13 @@ namespace AdventureGameDeclanFeoreJamesKoch
 {
     public partial class Form1 : Form
     {
-        // tracks what part of the game the user is at
+        //Tracks what part of the game the user is at.
         int scene = 0;
 
-        // random number generator
+        //Random number generator.
         Random randGen = new Random();
 
-        //all sounds in the game
+        //All sounds in the game.
         SoundPlayer carCrash = new SoundPlayer(Properties.Resources.car_crash);
         SoundPlayer carEngine = new SoundPlayer(Properties.Resources.car_engine);
         SoundPlayer goatScream = new SoundPlayer(Properties.Resources.goat_scream);
@@ -31,7 +31,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
         {
             InitializeComponent();
             
-            //display initial message and options
+            //Display initial message and options.
             outputLabel.Text = "You want to go to your friend's house. What mode of transportation will you use.";
             redLabel.Text = "Walk";
             blueLabel.Text = "Drive";
@@ -41,8 +41,8 @@ namespace AdventureGameDeclanFeoreJamesKoch
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            /// check to see what button has been pressed and advance to the next appropriate scene.
-            if (e.KeyCode == Keys.B) //red button press
+            //Check to see what button has been pressed and advance to the next appropriate scene.
+            if (e.KeyCode == Keys.B) //red/B button press.
             {
                 if (scene == 0) { scene = 1; }
                 else if (scene == 1) { scene = 3; }
@@ -71,7 +71,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
                 else if (scene == 101) { scene = 0; }
                 else if (scene == 102) { scene = 0; }
             }
-            else if (e.KeyCode == Keys.N) //blue button press
+            else if (e.KeyCode == Keys.N) //blue/N button press.
             {
                 if (scene == 0) { scene = 2; }
                 else if (scene == 1) { scene = 4; }
@@ -79,33 +79,33 @@ namespace AdventureGameDeclanFeoreJamesKoch
                 else if (scene == 3) { scene = 8; }
                 else if (scene == 4) { scene = 9; }
                 else if (scene == 5) { scene = 11; }
-                else if (scene == 6) { this.Close(); }
+                else if (scene == 6) { Thread.Sleep(1000); this.Close(); }
                 else if (scene == 7) { scene = 13; }
-                else if (scene == 8) { this.Close(); }
+                else if (scene == 8) { Thread.Sleep(1000); this.Close(); }
                 else if (scene == 9) { scene = 15; }
                 else if (scene == 10) { scene = 17; }
-                else if (scene == 11) { this.Close(); }
-                else if (scene == 13) { this.Close(); }
-                else if (scene == 14) { this.Close(); }
+                else if (scene == 11) { Thread.Sleep(1000); this.Close(); }
+                else if (scene == 13) { Thread.Sleep(1000); this.Close(); }
+                else if (scene == 14) { Thread.Sleep(1000); this.Close(); }
                 else if (scene == 16) { scene = 19; }
                 else if (scene == 17) { scene = 21; }
-                else if (scene == 18) { this.Close(); }
-                else if (scene == 21) { this.Close(); }
+                else if (scene == 18) { Thread.Sleep(1000); this.Close(); }
+                else if (scene == 21) { Thread.Sleep(1000); this.Close(); }
                 else if (scene == 99) { scene = 101; }
-                else if (scene == 100) { this.Close(); }
-                else if (scene == 101) { this.Close(); }
-                else if (scene == 102) { this.Close(); }
+                else if (scene == 100) { Thread.Sleep(1000); this.Close(); }
+                else if (scene == 101) { Thread.Sleep(1000); this.Close(); }
+                else if (scene == 102) { Thread.Sleep(1000); this.Close(); }
             }
-            else if (e.KeyCode == Keys.M) //yellow button press
+            else if (e.KeyCode == Keys.M) //yellow/M button press.
             {
                 if (scene == 0) { scene = 0; }
                 else if (scene == 6) { scene = 5; }
             }
 
-            /// Display text and game options to screen based on the current scene
+            //Display text and game options to screen based on the current scene.
             switch (scene)
             {
-                case 0:
+                case 0: //Starting scene.
                     outputLabel.Text = "You want to go to a friends house. What mode of transportatation do you use?";
                     redLabel.Text = "Walk";
                     blueLabel.Text = "Drive";
@@ -114,7 +114,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     mLabel.Visible = true;
                     sceneImage.BackgroundImage = Properties.Resources.case_0;
                     break;
-                case 1:
+                case 1: //Deciding what way to walk.
                     outputLabel.Text = "Do you walk the long safe way or the short scary way?";
                     redLabel.Text = "Long Safe Way";
                     blueLabel.Text = "Short Scary Way";
@@ -123,7 +123,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     dramatic.Play();
                     sceneImage.BackgroundImage = Properties.Resources.case_1_real;
                     break;
-                case 2:
+                case 2: //Choosing which car to drive.
                     outputLabel.Text = "What car do you drive? 2002 Honda Civic or 2019 Ferrari";
                     redLabel.Text = "2002 Honda Civic";
                     blueLabel.Text = "2019 Ferraria";
@@ -132,21 +132,21 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     sceneImage.BackgroundImage = Properties.Resources.case_2;
                     carEngine.Play();
                     break;
-                case 3:
+                case 3: //Choosing whether to keep walking or starting running.
                     outputLabel.Text = "It is taking too long just walking and you become impatient. Do you begin to run or continue walking?";
                     redLabel.Text = "Walk";
                     blueLabel.Text = "Run";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_3_realerson;
                     break;
-                case 4:
+                case 4: //Do you check out the sound or run away from it.
                     outputLabel.Text = "You are walking down by the river and hear 'help!'. Do you check it out or run away?";
                     redLabel.Text = "Run away";
                     blueLabel.Text = "Check it out";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_4;
                     break;
-                case 5:
+                case 5: //Get or skip food.
                     outputLabel.Text = "Do you go get food on your way to your friends, or skip food and go right to your friends?";
                     redLabel.Text = "Get food";
                     blueLabel.Text = "Skip food";
@@ -156,7 +156,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_5;
                     break;
-                case 6:
+                case 6: //Random chance at crashing the ferrari if you picked it.
                     int chance = randGen.Next(1, 11);
                     if (chance < 8)
                     { outputLabel.Text = "You chose the Ferrari!"; yellowLabel.Text = "Continue"; blueLabel.Text = ""; redLabel.Text = ""; mLabel.Visible = true; nLabel.Visible = false; bLabel.Visible = false; sceneImage.BackgroundImage = Properties.Resources.case_6_real; }
@@ -165,7 +165,7 @@ namespace AdventureGameDeclanFeoreJamesKoch
                         carCrash.Play();
                     }
                     break;
-                case 7:
+                case 7: //Do you drink from the puddle.
                     outputLabel.Text = "You get thirsty and see a puddle on the road. Do you take a drink from it?";
                     redLabel.Text = "Yes";
                     blueLabel.Text = "No";
@@ -175,14 +175,14 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_7;
                     break;
-                case 8:
+                case 8: //Tripped over a stick and died. Given the option to play again or exit game.
                     outputLabel.Text = "You trip over a stick, hit your head, and internally bleed to death! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_8;
                     break;
-                case 9:
+                case 9: //Do you pet the screaming goat.
                     outputLabel.Text = "Oh my goodness! It is a screaming goat! Do you pet it?";
                     redLabel.Text = "Yes";
                     blueLabel.Text = "No";
@@ -190,21 +190,21 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     sceneImage.BackgroundImage = Properties.Resources.case_9;
                     goatScream.Play();
                     break;
-                case 10:
+                case 10: //Choosing what place to go to for food.
                     outputLabel.Text = "What food store do you go to? Subway or Louisiana Chicken?";
                     redLabel.Text = "Subway";
                     blueLabel.Text = "Louisiana Chicken";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_10;
                     break;
-                case 11:
+                case 11: //Died of hunger. Given the option to play again or exit game.
                     outputLabel.Text = "You die of hunger! Play again?";
                     redLabel.Text = "Play Again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_11;
                     break;
-                case 12:
+                case 12: //Made it nice and hydrated to your friend's house.
                     outputLabel.Text = "You make it to your friends house nice and hydrated.";
                     redLabel.Text = "Continue";
                     blueLabel.Text = "";
@@ -212,21 +212,21 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_12;
                     break;
-                case 13:
+                case 13: //Died of thirst. Given the option to play again or exit game.
                     outputLabel.Text = "You die of thirst! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_13;
                     break;
-                case 14:
+                case 14: //The goat ate you. Given the option to play again or exit game.
                     outputLabel.Text = "The goat gets mad and eats you painfully! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_14;
                     break;
-                case 15:
+                case 15: //Continued walking to friends.
                     outputLabel.Text = "You continue walking to your friends house, but the goat stares at you menacingly.";
                     redLabel.Text = "Continue";
                     blueLabel.Text = "";
@@ -234,36 +234,36 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_15_real;
                     break;
-                case 16:
+                case 16: //Choosing what sandwich to get at Subway.
                     outputLabel.Text = "Do you get a tuna sub or BLT?";
                     redLabel.Text = "Tuna sub";
                     blueLabel.Text = "BLT";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_16_sub;
                     break;
-                case 17:
+                case 17: //Choosing what meal to get at Louisiana Chicken.
                     outputLabel.Text = "Do you get Popcorn chicken or Chicken tenders? ";
                     redLabel.Text = "Popcorn chicken";
                     blueLabel.Text = "Chicken tenders";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_17;
                     break;
-                case 18:
+                case 18: //Died of mercury poisoning. Given the option to play again or exit game.
                     outputLabel.Text = "You die of mercury poisoning! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case18;
                     break;
-                case 19:
-                    outputLabel.Text = "After eating you drive to your friends.";
+                case 19: //Drove to your friend's house.
+                    outputLabel.Text = "After eating you drive to your friend's home.";
                     redLabel.Text = "Continue";
                     blueLabel.Text = "";
                     nLabel.Visible = false;
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_18;
                     break;
-                case 20:
+                case 20: //Loved the popcorn chicken.
                     outputLabel.Text = "The popcorn chicken is so good and filling!";
                     redLabel.Text = "Continue";
                     blueLabel.Text = "";
@@ -271,14 +271,14 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.Shridar_Patel;
                     break;
-                case 21:
+                case 21: //Died from salmonella poisoning. Given the option to play again or exit game.
                     outputLabel.Text = "The chicken tenders are under cooked, and you get salmonella poisoning and die! Play again";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_21;
                     break;
-                case 99:
+                case 99: //Do you walk in the door or walk first.
                     outputLabel.Text = "When you get there, do you walk right in or knock on the door first?";
                     redLabel.Text = "Walk right in";
                     blueLabel.Text = "Knock first";
@@ -286,21 +286,21 @@ namespace AdventureGameDeclanFeoreJamesKoch
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_99;
                     break;
-                case 100:
+                case 100: //Got shot by your friend because he thought you were an intruder. Given the option to play again or exit game.
                     outputLabel.Text = "You get shot by your friend because he confused you for an intruder! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_100;
                     break;
-                case 101:
+                case 101: //Winning scene.
                     outputLabel.Text = "You have fun with your friend! Thanks for playing! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
                     yellowLabel.Text = "";
                     sceneImage.BackgroundImage = Properties.Resources.case_101;
                     break;
-                case 102:
+                case 102: //Skipped everything.
                     outputLabel.Text = "You're boring! Play again?";
                     redLabel.Text = "Play again";
                     blueLabel.Text = "Exit";
